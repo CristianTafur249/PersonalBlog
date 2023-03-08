bd1e2f04e8399e3aa30f57b923cd480ba7e68923
CristianTafur249 <cristian2191970@correo.uis.edu.co>
2023-03-07 23:04:46 -0500

    primer commit

---
+++ data/siteMetatdata.js
@@ -0,0 +1,18 @@
+const siteMetadata={
+    site_name: 'Tafur Blog',
+    title: 'The TechCode',
+    author: 'Cristian Tafur',
+    headerTitle: 'The TechCode',
+    description: 'Código, tecnología y programación',
+    language: 'es-ES',
+    email: 'cristiantafur@gmail.com',
+    siteUrl: '',
+    socialBanner: '/static/images/twitter-card.png',
+    siteRepo:'https://github.com/CristianTafur249/blog-personal.git',
+    siteLogo: '/static/images/logo-techcode.png',
+    twitter: 'https://twitter.com/Cristia16195614',
+    linkedin: 'https://www.linkedin.com/in/cristaf/',
+    locale: 'es-CO',
+    github:'https://github.com/CristianTafur249/'
+};
+module.exports = siteMetadata
