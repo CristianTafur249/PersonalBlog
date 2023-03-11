105febe4c4132f1c48d6ac7f5fd2385dd02f97a5
CristianTafur249 <cristian2191970@correo.uis.edu.co>
2023-03-10 19:44:54 -0500

    agregado de librerias y documentos nocesarios, ademas del sobre mi

---
+++ components/Tag.js
@@ -0,0 +1,13 @@
+const { default: Link } = require("next/link")
+
+
+const Tag =({text})=>{
+    return(
+        <Link href={`/tags/${text}`} legacyBehavior>
+            <a className="relative z-10 rounded-full  py-1.5 px-3 font-medium text-green-600 hover:text-blue-400">
+                {text.split(" ").join('-')}
+            </a>
+        </Link>
+    )
+}
+export default Tag;
---
+++ data/authors/Tafur.md
@@ -0,0 +1,8 @@
+---
+name: Cristian Tafur
+avatar: 
+occupation: Estudiante Ingenieria de Sistemas UIS
+twitter: https://mobile.twitter.com/Cristia16195614
+linkedin: https://www.linkedin.com/in/cristaf/
+github: 
+---
