b48b1311bec9a4a72cc3e4730e8e389a2d8bb659
CristianTafur249 <cristian2191970@correo.uis.edu.co>
2023-03-26 09:32:52 -0500

    prueba

--- pages/index.js
+++ pages/index.js
@@ -41,6 +41,7 @@ export default function Home({ posts }) {
                         {title}
                       </a>
                     </h3>
+                    
                     <div className=' justify-start mb-3 flex flex-wrap'>
                       {tags.map((tag) => (
                         <Tag clas={"transition ease-in-out duration-700 hover:border-b-2 relative z-10 rounded-full m-auto ml-2 my-1  text-center  border-green-700 text-green-700 hover:border-gray-800 hover:text-blue-800 dark:hover:border-gray-50 dark:hover:text-blue-50 "} key={tag} text={tag} />
