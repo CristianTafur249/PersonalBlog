b62570a033d12319ba50b7b6280d0de7be939ad3
CristianTafur249 <cristian2191970@correo.uis.edu.co>
2023-04-14 00:28:13 -0500

    arreglos varios, insercion del blog "instalacion conky", "instalacion directorio compartido"

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,6 +1,6 @@
 const projectData = [
   {
-    title: 'ThecCode',
+    title: 'ThechCode',
     description: `TechCode es un blog personal dedicado a la tecnología y el código. En este espacio, encontrarás contenido sobre desarrollo de software, programación, tecnología, noticias de la industria y más.
         ¡Gracias por visitar TechCode! Espero que disfrutes del contenido y te animes a participar en la conversación. Si tienes alguna pregunta, comentario o sugerencia, no dudes en ponerte en contacto conmigo.`,
     imgSrc: '/static/images/TechCode1.png',
