52124dcd4c8600da0ae1106b9bead100166e8eec
CristianTafur249 <tafurcristian196@gmail.com>
2024-01-06 23:33:35 -0500

    Add less package and update description in etiquetas.js

--- pages/etiquetas.js
+++ pages/etiquetas.js
@@ -1,5 +1,4 @@
 import { PageSEO } from '@/components/SEO'
-import siteMetadata from '@/data/siteMetatdata'
 import { getAllTags } from '@/lib/tags'
 import Tag from '@/components/Tag'
 import Link from 'next/link'
@@ -15,7 +14,7 @@ export default function Tags({ tags }) {
   const sortedTags = Object.keys(tags).sort((a, b) => tags[b] - tags[a])
   return (
     <>
-      <PageSEO title={`Etiquetas`} description="Etiqutas de lo que blogueo" />
+      <PageSEO title={`Etiquetas`} description="Etiquetas del que blog" />
       <div className="mx-auto my-auto w-4/5  px-6 lg:px-8">
         <section className="mb-16 text-center lg:text-left">
           <div className="border-b-2 grid lg:grid-cols-2 gap-6 xl:gap-12 items-center">
@@ -27,13 +26,13 @@ export default function Tags({ tags }) {
           </div>
         </section>
         <div className=" rounded-full grid grid-cols-2 gap-6 lg:grid-cols-3 mb-20 justify-items-start items-center text-sm md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
-          {Object.keys(tags).length === 0 && 'No se encontraron etiqutas.'}
+          {Object.keys(tags).length === 0 && 'No se encontraron etiquetas.'}
           {sortedTags.map((t) => {
             return (
               <div key={t} className="-mt-2 m-auto  mb-2 w-full mr-5">
                 <Tag
                   text={t}
-                  clas="transition ease-in-out duration-700 hover:border-b-2 mx-2 rounded-full uppercase relative z-10  m-auto my-1 p-1 text-center text-green-700 dark:text-green-400 hover:border-gray-800 hover:text-blue-800"
+                  class="transition ease-in-out duration-700 hover:border-b-2 mx-2 rounded-full uppercase relative z-10  m-auto my-1 p-1 text-center text-green-700 dark:text-green-400 hover:border-gray-800 hover:text-blue-800"
                 />
                 <Link href={`/etiquetas/${formatWord(t)}`} className="-ml-2 font-semibold ">
                   {`(${tags[t]})`}
